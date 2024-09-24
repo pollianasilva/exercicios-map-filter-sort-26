a182d84d5e79e3cc9656b06c2d173cc6bbc7bc51
pollianasilva <polly.cr@gmail.com>
2024-09-24 13:49:11 -0300

    feat: retornar reprovadas

--- src/implementacao/3-retornar-reprovadas.ts
+++ src/implementacao/3-retornar-reprovadas.ts
@@ -1,3 +1,26 @@
-function retornarReprovadas() {
-    return null;
+/* 3) Fazer uma função que retorne um array de nome das reprovadas*/
+
+
+function retornarReprovadas(): string [] {
+    const alunos =[
+        {nome: 'Chico Bento', media: 4},
+        {nome: 'Rosinha', media: 9},
+        {nome: 'Cascão', media: 3},
+        {nome: 'Cebolinha', media: 6},
+        {nome: 'Magali', media: 8},
+        {nome: 'Mônica', media: 7}
+
+    ];
+
+    const reprovadas = alunos.filter(aluno => aluno.media <= 4)
+        .map(aluno => aluno.nome); 
+
+
+    return reprovadas;
 }
+
+
+//teste
+
+console.log(retornarReprovadas());
+
