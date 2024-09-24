9395411f1de2a75bfc7f7058f4a4458673ef47ad
pollianasilva <polly.cr@gmail.com>
2024-09-24 13:40:59 -0300

    feat: retornar aprovadas

--- src/implementacao/2-retornar-aprovadas.ts
+++ src/implementacao/2-retornar-aprovadas.ts
@@ -1,4 +1,25 @@
-function retornarAprovadas() {
-    return null;
+/* 2) Fazer uma função que retorne um array de nomes das aprovadas */
+
+
+function retornarAprovadas(): string [] {
+    const alunos =[
+        {nome: 'Chico Bento', media: 5},
+        {nome: 'Rosinha', media: 9},
+        {nome: 'Cascão', media: 10},
+        {nome: 'Cebolinha', media: 6},
+        {nome: 'Magali', media: 8},
+        {nome: 'Mônica', media: 7}
+
+    ];
+
+    const aprovadas = alunos.filter(aluno => aluno.media >= 7)
+        .map(aluno => aluno.nome); 
+
+
+    return aprovadas;
 }
+
+//teste
+
+console.log(retornarAprovadas());
 
