caa0ab14ef5335c25c3c6e38aaf996a2dab47b34
pollianasilva <polly.cr@gmail.com>
2024-09-24 13:09:33 -0300

    feat: retornar medias

--- src/implementacao/1-retornar-medias.ts
+++ src/implementacao/1-retornar-medias.ts
@@ -1,3 +1,16 @@
-function retornarMedias(){
-    return null
+/* 1) Fazer uma função que retorne um array de todas as médias */
+
+
+function retornarMedias(): number[] {
+
+    const media = [10, 9, 7, 6, 5, 8];
+    const mediaAprovados = media.filter(num => num >= 7)
+
+    return mediaAprovados
 }
+
+
+//teste
+
+console.log(retornarMedias());
+
