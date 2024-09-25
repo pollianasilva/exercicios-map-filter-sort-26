a4d5c4f72f155f245b1c889af05eb6dccefcad02
pollianasilva <polly.cr@gmail.com>
2024-09-25 13:45:10 -0300

    feat: retornar maior media

--- src/implementacao/5-retornar-maior-media.ts
+++ src/implementacao/5-retornar-maior-media.ts
@@ -1,3 +1,33 @@
-function retornarMaiorMedia() {
-    return null;
+/*5) Fazer uma função que retorne o nome da aluna com maior nota*/
+
+
+function retornarMaiorMedia():string {
+
+    const alunas =[
+        { nome: "Ashley", prova: { p1: 5.6, p2: 6.7, p3: 9 } },
+        { nome: "Sabrina", prova: { p1: 6.3, p2: 7.5, p3: 10 } },
+        { nome: "Samantha", prova: { p1: 3, p2: 6.2, p3: 6 } }
+    ];
+
+const resultado = alunas.map(aluna => {
+  
+    const soma = aluna.prova.p1 + aluna.prova.p2 + aluna.prova.p3;   
+    const media = soma / 3;   
+      
+    
+    return {
+      nome: aluna.nome,
+      media: Number(media.toFixed(1)), 
+      
+    };
+  });
+
+  resultado.sort((a, b) => b.media - a.media);
+
+  return resultado[0].nome;
+  
 }
+
+
+// Teste
+console.log(retornarMaiorMedia());
